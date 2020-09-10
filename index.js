126fda8eb21b25a5f5943ebd0b5adad7174eefcb
Troy A. Murray <prodbysessions@gmail.com>
2020-09-09 22:25:45 -0400

    questions

--- index.js
+++ index.js
@@ -78,11 +78,9 @@ Your function should remove a flavor from the end of the array and console.log t
 
 For example removeLastFlavor(originalFlavors) would return ["Rainbow Sherbert", "Banana Nut Fudge",..."Vanilla"]*/ 
 
-function removeLastFlavor(/*code here*/){
+originalFlavors.pop();
 
-    /*code here*/
-
-}
+console.log(originalFlavors);
 
 /* Task 4: Write a function that returns a flavor at a given index in the array.
 
@@ -93,9 +91,9 @@ Your function should accept:
 
 For example, getFlavorByIndex(originalFlavors, 2) would return "Black Walnut", assuming Rainbow Sherbert has been added successfully. */
 
-function getFlavorByIndex(/*code here*/){
+function getFlavorByIndex(originalFlavors, 2){
 
-    /*code here*/
+   
 
 }
 
@@ -127,11 +125,16 @@ Your function should accept:
 
 and should return a new array that is identical to the old array. You can name the new array however you'd like. */
 
-function copy(/*code here*/){
+let newArr = [];
 
-    /*code here*/
+function copy(newArrPlace, originalArr){
+
+    newArrPlace = [...originalArr]
+    return newArrPlace;
 
 }
+
+console.log(copy(newArr, originalFlavors));
 
 /* Task 7: July 7th is "World Chocolate Day" and Baskin Robins wants to create promotional materials highlighting all of their chocolate flavors. Write a function that checks every item in the array for a given string and returns a new array called filteredArray with just these values. Rather than hardcoding "chocolate" into your function, pass a string as a parameter, and invoke with the argument "chocolate". This way you could also filter for "Vanilla", "Sherbert", etc. when those holidays roll around.
 
